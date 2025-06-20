2fb90a47b9907e317b926809d6541e5031c323b6
raininfotech-mihir <mihir197@webmail.rainit.in>
2025-06-20 17:36:27 +0530

    MD | Update the server.jssssssssssssssssssssssssssssssss
    Hey there

--- server.js
+++ server.js
@@ -5,11 +5,7 @@ const socketIo = require("socket.io");
 const router = require("./routes/v1");
 const dbConnect = require("./helpers/dbConnect");
 const { addSocket, removeSocket } = require("./helpers/socketStore");
-const app = express();
-const server = http.createServer(app);
-    let userId = socket.handshake.auth.token;
-    if (userId) {
-        addSocket(decWithoutSymbol(userId.toString(), keyStore('userId')), socket.id);
+const app = express();ket.id);
         console.log(`[+] User ${userId} connected with socket
 });
 
