a30d167f6d129141ab83e22377422b4b5eb77f70
raininfotech-mihir <mihir197@webmail.rainit.in>
2025-06-21 03:47:36 +0530

    Update server.js
    Remove socket from server.js file because it is not working proper

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const http = require("http");
-const socketIo = require("socket.io");
 const router = require("./routes/v1");
 const dbConnect = require("./helpers/dbConnect");
 const { addSocket, removeSocket } = require("./helpers/socketStore");
